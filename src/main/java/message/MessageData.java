ee2dd30b11fc631c4da403f641e6269447bc9eb2
aliceiwill <jw_eric@163.com>
2018-11-30 09:59:45 +0800

    用户评价功能的嵌入
    两个接入口：
    一是首页的一栏，用户评价
    一是个人中心—评价商品—有图评价
    
    引入，message包
    引入三个jsp文件
    更新comment.jsp文件，index.jsp,home2.jsp
    
    另外配套数据库图片已经上传

---
+++ src/main/java/message/MessageData.java
@@ -0,0 +1,14 @@
+package message;
+
+
+public class MessageData 
+ {	private String name,email,title,content;
+	//setter或者getter方法
+	public void setName(String name){this.name=name;}
+	public void setEmail(String email){	this.email=email;}
+	public void setTitle(String title){	this.title=title;}
+	public void setContent(String content){this.content=content;}
+	public String getName(){ return this.name;}
+	public String getContent(){	return this.content;}
+	public String getTitle(){ return this.title;}
+	public String getEmail(){ return this.email;}}
