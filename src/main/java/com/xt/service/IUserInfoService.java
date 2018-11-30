04d21682867b3c0e4eeae2d9352bb9ea0dc4e02b
李岚祺 <42244102+LiLanQi@users.noreply.github.com>
2018-11-30 19:42:37 +0800

    Revert "Revert "Revert "更新1"""
    This reverts commit 7ce0c4e7b3af0120d1000d65d9bcf69b88eb1f52.

--- src/main/java/com/xt/service/IUserInfoService.java
+++ src/main/java/com/xt/service/IUserInfoService.java
@@ -17,9 +17,7 @@ public interface IUserInfoService {
 	 * 
 	 * @author 李岚祺
 	 */
-	List<User> loginById(String userName,String password);
-	List<User> loginByEmail(String userName, String password);
-	List<User> loginByMobile(String userName, String password);
+	List<User> login(String userName,String password);
 	
 	/**
 	 * 依据密码和手机号注册抽象方法
@@ -36,6 +34,4 @@ public interface IUserInfoService {
 	boolean registerByEmail(String password, String email);
 	
 	void updateUserInformation(String id, String tempName, String realName, int sex, Timestamp date, String email, String phone);
-	
-	void updataAddressInformation(String id, String name, String phone, String address, String detailAddress);
 }
